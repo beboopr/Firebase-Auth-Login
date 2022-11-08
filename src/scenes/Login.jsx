27089ccb860e6e719291abece1d9db6f0d3458b9
beboopr <linnets-router.09@icloud.com>
2022-11-08 11:35:19 -0500

    auth done with singin

--- src/scenes/Login.jsx
+++ src/scenes/Login.jsx
@@ -1,10 +1,28 @@
 import { useState } from "react"
+import { initializeApp } from 'firebase/app'
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
 
-export default function Login() {
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCFgd8rDKfR1wzWdV0lHQnYIR3z1k8ZC54",
+    authDomain: "fir-auth-captain.firebaseapp.com",
+    projectId: "fir-auth-captain",
+    storageBucket: "fir-auth-captain.appspot.com",
+    messagingSenderId: "575644524618",
+    appId: "1:575644524618:web:27003d374ecee45767c6b3"
+  };
+
+export default function Login( {setUser} ) {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
-    const handleLogin = (e) => {
+    const handleLogin = async (e) => {
         e.preventDefault()
+        const app = initializeApp(firebaseConfig) // connects to firebase
+        const auth = getAuth(app) // connect us to firebase Auth
+        const response = await signInWithEmailAndPassword(auth, email, password) // sing us in
+        .catch(alert)
+        setUser(response.user)
+
     }
     return (
         <>
