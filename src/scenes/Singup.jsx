f40e2101fffc57eb9e754df8c6f5e10d274415d6
beboopr <linnets-router.09@icloud.com>
2022-11-08 11:45:53 -0500

    sing in with Google

--- src/scenes/Singup.jsx
+++ src/scenes/Singup.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react"
 import { initializeApp } from 'firebase/app'
-import { getAuth, signInWithEmailAndPassword, GoogleAuthProvider, 
-    signInWithPopup } from 'firebase/auth'
+import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth'
 
 
 const firebaseConfig = {
@@ -13,30 +12,22 @@ const firebaseConfig = {
     appId: "1:575644524618:web:27003d374ecee45767c6b3"
   };
 
-export default function Login( {setUser} ) {
+export default function Singup( {setUser} ) {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
-    const handleLogin = async (e) => {
+    const handleSingup = async (e) => {
         e.preventDefault()
         const app = initializeApp(firebaseConfig) // connects to firebase
         const auth = getAuth(app) // connect us to firebase Auth
-        const response = await signInWithEmailAndPassword(auth, email, password) // sing us in
+        const response = await createUserWithEmailAndPassword(auth, email, password) // sing us in
         .catch(alert)
         setUser(response.user)
 
     }
-    const handleGoogleLogin = async () => {
-        const app = initializeApp(firebaseConfig) // connects to firebase
-        const auth = getAuth(app) // connect us to firebase Auth
-        const provider = new GoogleAuthProvider()
-        const response = await signInWithPopup(auth, provider)
-        .catch(alert)
-        setUser(response.user)
-    }
     return (
         <>
-        <h1>Login</h1>
-        <form onSubmit={handleLogin}>
+        <h1>Singup</h1>
+        <form onSubmit={handleSingup}>
             <label htmlFor="email">Email:{' '}
             <input type="email" name="email" 
             value = {email} onChange={e => setEmail(e.target.value)}
@@ -47,10 +38,8 @@ export default function Login( {setUser} ) {
             value = {password} onChange={e => setPassword(e.target.value)}
             placeholder="your password" />
             </label><br />
-            <button type="submit">Login</button>
+            <button type="submit">Singup</button>
         </form>
-        <br />
-        <button onClick={handleGoogleLogin}>Sing in with Google</button>
         </>
     )
 }
